fe59f85b9c7db1db01683b7a10980c7c814622e7
a a <k024g1016@g.neec.ac.jp>
2025-11-28 10:42:11 +0900

    途中

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    private TMP_Text scoreText;
+    private ARWeapon aRWeapon;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SetScore(int score)
+    {
+
+    }
+
+    private void UpdateScoreText()
+    {
+
+    }
+}
